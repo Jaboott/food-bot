5db7f730eb3f829d3b2c56298055255132054253
Jason Xu <jasonxuuuu@gmail.com>
2024-07-20 22:49:40 -0700

    Created the constructor for DatabaseManager that establish connection with database.

--- src/main/java/database/DatabaseManager.java
+++ src/main/java/database/DatabaseManager.java
@@ -1,4 +1,20 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseManager {
+
+    private Connection connection;
+    public DatabaseManager(String path) {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + path);
+            System.out.println("SQLite Connection established");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
 }
