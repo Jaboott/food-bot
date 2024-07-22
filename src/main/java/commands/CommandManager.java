88dcb07a0ee6109e79cbefaa11bd34310069bbba
Jason Xu <jasonxuuuu@gmail.com>
2024-07-21 19:10:11 -0700

    Added additional fields required by the add command to reflect the addition of Restaurant class

--- src/main/java/commands/CommandManager.java
+++ src/main/java/commands/CommandManager.java
@@ -3,6 +3,9 @@ package commands;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import restaurant.Cuisine;
+import restaurant.GeneralLocation;
 
 public class CommandManager {
 
@@ -16,10 +19,29 @@ public class CommandManager {
                 Commands.slash("echo", "Repeats messages back to you.")
                         .addOption(OptionType.STRING, "message", "The message to repeat."),
                 Commands.slash("add", "Add a restaurant to the bot.")
-                        .addOption(OptionType.STRING, "restaurant", "The name of the restaurant."),
+                        .addOption(OptionType.STRING, "restaurant", "The name of the restaurant.", true)
+                        .addOptions(buildCuisine())
+                        .addOptions(buildGeneralLocation()),
                 Commands.slash("random", "Chooses a random restaurant for you.")
         ).queue();
 
         System.out.println("Finish loading commands");
+    }
+
+
+    private OptionData buildCuisine() {
+        OptionData optionData = new OptionData(OptionType.STRING, "type", "The type of cuisine", true);
+        for (Cuisine c : Cuisine.values()) {
+            optionData.addChoice(c.getType() + " Cuisine", c.name());
+        }
+        return optionData;
+    }
+
+    private OptionData buildGeneralLocation() {
+        OptionData optionData = new OptionData(OptionType.STRING, "location", "The general location of the restaurant", true);
+        for (GeneralLocation l : GeneralLocation.values()) {
+            optionData.addChoice(l.getLocation(), l.name());
+        }
+        return optionData;
     }
 }
