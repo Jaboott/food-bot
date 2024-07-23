24ed9fa762f56476f32c5a00ca7e5c3178a2fe37
Jason Xu <jasonxuuuu@gmail.com>
2024-07-23 00:31:39 -0700

    Added searchRestaurants method.

--- src/main/java/database/DatabaseManager.java
+++ src/main/java/database/DatabaseManager.java
@@ -4,16 +4,13 @@ import restaurant.Cuisine;
 import restaurant.GeneralLocation;
 import restaurant.Restaurant;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Random;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DatabaseManager {
 
     private Connection connection;
-    private Random random;
 
     public DatabaseManager() {
         this("data/restaurants.db");
@@ -22,7 +19,6 @@ public class DatabaseManager {
     public DatabaseManager(String path) {
         try {
             connection = DriverManager.getConnection("jdbc:sqlite:" + path);
-            random = new Random();
             System.out.println("SQLite Connection established");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -39,7 +35,7 @@ public class DatabaseManager {
             // Connection won't commit until both update is done successfully
             connection.setAutoCommit(false);
 
-            try (var pstmt = connection.prepareStatement(queryMain)) {
+            try (var pstmt = connection.prepareStatement(queryMain, Statement.RETURN_GENERATED_KEYS)) {
                 pstmt.setString(1, restaurant.getName());
                 pstmt.setString(2, restaurant.getNormalizedName());
                 pstmt.setString(3, restaurant.getType());
@@ -82,7 +78,7 @@ public class DatabaseManager {
 
     }
 
-    public Restaurant getRestaurant() throws SQLException{
+    public Restaurant getRestaurant() throws SQLException {
         String query = "SELECT * FROM restaurants ORDER BY RANDOM() LIMIT 1";
         try (var pstmt = connection.prepareStatement(query)){
             var result = pstmt.executeQuery();
@@ -94,6 +90,30 @@ public class DatabaseManager {
             return restaurant;
         } catch (SQLException e) {
             System.out.println("Failed to get restaurant");
+            throw e;
+        }
+    }
+
+    public List<Restaurant> searchRestaurants(String name) throws SQLException {
+        String query = "SELECT * FROM restaurants_fts WHERE name MATCH ?";
+        System.out.printf("Searching for name: %s%n", name);
+        try (var pstmt = connection.prepareStatement(query)) {
+            pstmt.setString(1, name);
+            var rs = pstmt.executeQuery();
+
+            List<Restaurant> restaurants = new ArrayList<>();
+
+            // Building the list of restaurants
+            while(rs.next()) {
+                String restaurant = rs.getString("name");
+                Cuisine cuisine = Cuisine.valueOf(rs.getString("cuisine"));
+                GeneralLocation generalLocation = GeneralLocation.valueOf(rs.getString("location"));
+                restaurants.add(new Restaurant(restaurant, cuisine, generalLocation));
+            }
+
+            return restaurants;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
             throw e;
         }
     }
